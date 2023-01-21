7534c36db00399be1092cc7c24aca95e2408080c
Alex <alexon249@gmail.com>
2023-01-22 01:30:59 +0300

    Fina

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# cryptosignals
-python script that exports cryptosignals to json
+# Telegram crypto scrapper
+Parse/grab/scrap cryptosignals in Telegram channels, sort it and save in MongoDB and works on Telethon
+
+## You wanna know
+
+This isn't pet-project, I made this as order. Production build I cannot to upload (NDA), but some dev version I allowed to show. This code supplied "as is" and I just want to share this because it has "line-by-line" :ru: comments and maybe help you to understand Python, MongoDB, parsing, sorting etc. [Telegram me!](https://iamradaken.t.me/) :ru: :us:
+
+
+## How to use it?
+
+1. Create config.ini and enter to it data like
+
+```
+[Telegram]
+
+api_id = 123456
+api_hash=123456asdgh-your-hash
+
+phone = +1235556677
+username = iamradaken
+
+#you can add comment with hash symbol
+```
+
+_You probably need to create telegram client. You can do this in [Telegram API Devtools](htttps://my.telegram.org)_
+
+2. Install reqs w/ command
+
+```
+pip -r install requirements.txt
+```
+3. Download and install MongoDB. _As default script configured on localhost server with default port_
+
+4. Open MongoDB Compass for monitoring stuff
+5. Enter your channels in `channels[]` array in script
+6. `python main.py`
+7. Enjoy in MondoDB Compass
+
+## Fork it and try by yourself!
+- [ ] Rewrite code to async telethon lib
+- [ ] --inline-args parser (e.g.: `python main.py --channels 'supercrypto' 'hypersignals'`)
+- [ ] junk and duplicate cleaner (I think this should be CRON job)
+- [ ] Parse more data, like entry points and add chatid to stream
+- [ ] Make protection from 420 FloodWaitError
+- [ ] Make multiaccount and proxy (for fault tolerance)
+- [ ] Replace config.ini to .env
--- main.py
+++ main.py
@@ -1,167 +1,153 @@
-import configparser
-import json
-from telethon.sync import TelegramClient, events
-from telethon.errors import SessionPasswordNeededError
-from telethon.tl.functions.channels import JoinChannelRequest
-from telethon.tl.functions.channels import GetMessagesRequest
-from telethon import functions, types
-import pymongo
+import configparser                                             #Библиотека парсера config.ini конфигурационного файла
+
+from telethon.sync import TelegramClient, events                #Библиотека для синхронной обработки кода и событий
+from telethon.errors import SessionPasswordNeededError          #Библиотека для обработки исключений (если на аккаунте 2FA)
+from telethon.tl.functions.channels import JoinChannelRequest   #Библиотека для входа в канал
+from telethon.tl.functions.channels import GetMessagesRequest   #Библиотека для получения сообщений
+from telethon import functions, types                           #Получаем в общем все типы и функции
+import pymongo                                                  #Библиотека для MongoDB
+
+import datetime                                                 #Библиотека для даты и времени
 
 
 
-short_dict = {} #Dictionary for short
-mid_dict = {} #Dictionary for medium
-long_dict = {} #Dictionary for long
-all_dict = {} #Dictionary for all
-
-class Signal:
+class Signal:                        # Объявляем класс
  
-    def __init__(self):
-        self.signal_type = "undefined"      # long/short
-        self.sourceChannel = "undefined"    # Канал-источник    
-        self.lever = "undefined"            # Рычаг (х10/x2)
-        self.target = "undefined"           # Контрольные точки
-        self.stoploss = "undefined"         # Стоплосс
-        self.coin = "undefined"             # Валюта
-        self.date = "undefined"             # Дата
-    # def scrap_and_set_val(self, line, _dict, value):
-    #     for keyword in _dict:
-    #                     if line.find(keyword):
-    #                         try:
-    #                             self.value = line.split(':')[1].upper()
-    #                         except IndexError:
-    #                             self.value = line.upper()
-    #                         except:
-    #                             print('Throwing unknown exeption in scrap_and_set_val')
+    def __init__(self):              # Объявляем конструктор и туда вносим переменные класса
+        self.signal_type = None      # long/short
+        self.sourceChannel = None    # Канал-источник    
+        self.lever = None            # Рычаг (х10/x2)
+        self.stoploss = None         # Стоплосс
+        self.coin = None             # Валюта
+        self.date = None             # Дата
+        self.entry = None            # Entry
                             
-    def upload(self, mydb, mycol='mycol'):
-        mydb.mycol.insert(self.__dict__)
-def main():
+def main():                          # Главная функция
 
-    connection_url="mongodb://localhost:27017/"
-    dbclient=pymongo.MongoClient(connection_url)
-    dblist = dbclient.list_database_names()
-    mydb = dbclient["cryptosignals"]
-    mycol = mydb["customers"]
-    mydb.mycol.insert_one({"test": 'test'})
-    dblist = dbclient.list_database_names()
-    if "mydatabase" in dblist:
-        print("The database exists.")
+    connection_url="mongodb://localhost:27017/"         #Адрес для монги
+    dbclient=pymongo.MongoClient(connection_url)        #Создаём клиент
+    mydb = dbclient["cryptosignals"]                    #Говорим, какую базу данных будем использовать
+    mycol = mydb["junk"]                                #Говорим, какой столбец будем использовать (по умолчанию junk), если не сработает сортировка
     
-    # Reading Configs
-    config = configparser.ConfigParser()
-    config.read("config.ini")
+    # Читаем конфиг
+    config = configparser.ConfigParser()                #создаём парсер конфига
+    config.read("config.ini")                           #название файла конфигурации
 
-    # Setting configuration values
-    api_id = config['Telegram']['api_id']
-    api_hash = config['Telegram']['api_hash']
+    # Задача значений из парсера: api_id, api_hash и т.д. данные конфиденциальны, поэтому мы должны доверять папке и среде, где храним конфиг
+    api_id = config['Telegram']['api_id']               #my.telegram.org > my apps
+    api_hash = config['Telegram']['api_hash']           #там же
 
-    api_hash = str(api_hash)
+    api_hash = str(api_hash)                            #явно преобразуем хэш в строку
 
     phone = config['Telegram']['phone']
     username = config['Telegram']['username']
 
-    # Create the client and connect
+    # Создаем клиент и подключаемся к Telegram
     client = TelegramClient(username, api_id, api_hash)
     client.start()
-    print("Client Created")
-    # Ensure you're authorized
+    print("Telegram client Created")
+    # EУдостоверяемся, что мы вошли
     client_auth_status = client.is_user_authorized()
     if not client_auth_status:
-        client.send_code_request(phone)
+        client.send_code_request(phone)                 #Если не вошли (нет файла %username%.session или возникли проблемы)
         try:
-            client.sign_in(phone, input('Enter the code: '))
-        except SessionPasswordNeededError:
-            client.sign_in(password=input('Password: '))
-    #channel = 'fatpig_cryptosignals'
-    channel = 'carprism'
-    client(JoinChannelRequest(channel))
-    channelMessages = client.get_messages(channel, limit=50)
+            client.sign_in(phone, input('Enter the code: '))        #Пробеум войти, требуем код подтверждения
+        except SessionPasswordNeededError:                          #Если поймали исключение о том, что стоит 2FA
+            client.sign_in(password=input('Password: '))            #Просим пароль
 
+    channels = ['carprism', 'coincodecap', 'fatpig_cryptosignals', 'Coin_Signals', 'coincodecap']  #Создаём лист каналов для парсинга
+    for channel in channels:
+        parseChannel(client, channel, mydb, mycol)     #Вызываем функцию ниже, куда передаём клиент, канал (в порядке итерации), базу данных и столбец по-умолчанию
     
-    triggersCoin = ['USDT', 'BTC', 'ETH', 'LTC', 'XRP', 'DOGE']
-    triggersType = ['LONG', 'Long', 'SHORT', 'Short']
-    triggersLever = ['LONG', 'Long', 'SHORT', 'Short']
-    triggersStoploss = ['Stoploss', 'Stop loss']
-    triggers = triggersCoin + triggersLever + triggersType + triggersLever + triggersStoploss
+#Фунция парсинга каналов
+def parseChannel(client, channel, mydb, mycol):
+    client(JoinChannelRequest(channel))               #Делаем запрос на вступление в канал
+    today = datetime.date.today()                     #Узнаем сегодняшнее число
+    week_ago = today - datetime.timedelta(days=7)     #Осчитываем неделю назад
+
+    channelMessages = client.get_messages(channel, limit=200, offset_date=week_ago)  #Записываем в переменную сообщения с канала за неделю с лимитом в 200 сообщ
+    triggersCoin = ['USDT', 'BTC', 'ETH', 'LTC', 'XRP', 'DOGE']                      #Интересующие коины
+    triggersType = ['LONG', 'Long', 'SHORT', 'Short']                                #Словарь типов сигнала
+    triggersLever = ['Lever', 'LEVER', 'Leverage', 'LEVERAGE']                       #Словарь для нахождения плеча
+    triggersStoploss = ['Stoploss', 'stoploss', 'Stop loss','STOP LOSS', 'Stop', 'stop']    #Словарь для стоп-позиции
+    triggers = triggersCoin + triggersLever + triggersType + triggersLever + triggersStoploss  #Делаем набор триггеров для нужного сообщения
     i=0
-    while (i != 10):        #Идёт прогонка (итерация) всех сообщений
-        
+    
+    while (i != len(channelMessages)):        #Идёт прогонка (итерация) всех сообщений
+    #Создаём экземпляр класса
+        signal_instance = Signal()
         #Поиск на совпадения по словарю имени крипты и позиции LONG/SHORT (2 значения, чтобы было меньше мусора)
         if isTriggered(triggersCoin, channelMessages[i].message) and isTriggered(triggersType, channelMessages[i].message):
-            #Создаём экземпляр класса
-            signal_instance = Signal()
-            print(f'Found this! in ', i)
+            
+            print(f'Found this! in ', i)    #Можно убрать, показываем в каком сообщении затриггерились
             #Для удобства обрабатываемое сообщение записываем в переменную message
             message = channelMessages[i].message
             #Начало поиска по строкам, разбивка строк
             for line in message.splitlines():
                 #Если в строке мы нашли любое значение связанное с криптой из словаря
-                if isTriggered(triggers, line):
-                    #print('Triggerd line ->', line)
-                    #print('***')
-                    #print(line.split(':')[1])
-                    
+                if isTriggered(triggersCoin, line):                                     # Поиск по коину
+                    try:
+                        signal_instance.coin = line.split(':')[1].upper()               # Запись КАПСОМ в класс нашего значения (после двоеточия)
+                    except IndexError:                                                  # Если двоеточия не нашли
+                        signal_instance.coin = line.upper()                             # , то записываем строку полностью
+                if isTriggered(triggersLever, line):                                    # Поиск по плечу
+                    try:
+                        signal_instance.lever = line.split(':')[1].upper()
+                    except IndexError:
+                        signal_instance.lever = line.upper()
+                if isTriggered(triggersType, line):                                     # Поиск по типу шорт/лонг
+                    try:
+                        signal_instance.signal_type = line.split(':')[1].upper()
+                    except IndexError:
+                        signal_instance.signal_type = line.upper()
+                if isTriggered(triggersStoploss, line):                                 # Поиск по стоп-позиции
+                    try:
+                        signal_instance.stoploss = line.split(':')[1].upper()
+                    except IndexError:
+                        signal_instance.stoploss = line.upper()
+                if 'Entry' in line:                                                     # Поиск по входным точкам, если есть
+                    try:
+                        signal_instance.entry = line.split(':')[1].upper()
+                    except IndexError:
+                        signal_instance.entry = line.upper()
+            signal_instance.date = channelMessages[i].date                             # Запись даты сообщения в формате date
+            signal_instance.sourceChannel = channel                                    # Запись канала, в котором нашли информацию
+        #ENDIF isTriggered by 2 keyword -- Для удобства чтения
+        try:
+            if (signal_instance.coin is None) or (len(signal_instance.coin) > 35):     # Если мы затриггерились, но коин не нашли, или его значения больше 35 символов
+                continue    # Не пишем в БД
+            else:
+                if Find(signal_instance.signal_type, 'LONG'):       #Иначе записываем ищем в строке с сигналом тип нашего сигнала
+                    mydb.LONG.insert_one(vars(signal_instance))     #Пишем в БД все переменные класса
+                if Find(signal_instance.signal_type, 'SHORT'):
+                    mydb.SHORT.insert_one(vars(signal_instance))
+                if Find(signal_instance.signal_type, 'MID'):
+                    mydb.MID.insert_one(vars(signal_instance))
+        except pymongo.errors.DuplicateKeyError:                   #Но если ID совпадает, то не пишем и выводим в консоль словленный exeption
+            print('DuplicateKeyException')
+        finally:                                                   # Но в любом случае удаляем наш класс и делаем шаг итерации
+            del signal_instance 
+            i=i+1
+    #ENDWHILE -- Для удобства чтения
 
-                    
-                    for keyword in triggersCoin:
-                        print ('KEYWORD IS' , keyword)
-                        if line.find(keyword):
-                            print ('FOUND ' , keyword, 'ON LINE ', line)
-                            try:
-                                signal_instance.coin = line.split(':')[1].upper()
-                                print ('WRITING IN CLASS THIS' , signal_instance.coin)
-                            except IndexError:
-                                signal_instance.coin = line.upper()
-                                print('EXEPTION WORKED WRITING ',signal_instance.coin )
-                            finally:
-                                #print(vars(signal_instance))
-                                break
+def isTriggered(triggers, message):            # Функция поиска триггеров в сообщении
+    if message is None:                        # Если сообщение пустое, то возвращаем ЛОЖЬ
+        return False
+    for trigger in triggers:                   # Проходимся по триггерам
+        try:
+            if (trigger in message):           # Если находим, возращаем триггер (а в условия любое значение триггера вернет нам ИСТИНА)
+                return trigger
+        except:
+            return False                       # Если возникнет ошибка: возвращаем ЛОЖЬ
 
-                    # for keyword in triggersLever:
-                    #     if line.find(keyword):
-                    #         signal_instance.lever = line.split(':')[1].upper()
-                    # for keyword in triggersType:
-                    #     if line.find(keyword):
-                    #         signal_instance.signal_type = line.split(':')[1].upper()
-                    # for keyword in triggersStoploss:
-                    #     if line.find(keyword):
-                    #         signal_instance.coin = line.split(':')[1].upper()
-                    
-                    # signal_instance.scrap_and_set_val(line, triggersCoin, 'coin')
-                    # signal_instance.scrap_and_set_val(line, triggersLever, 'lever')
-                    # signal_instance.scrap_and_set_val(line, triggersType, 'signal_type')
-                    # signal_instance.scrap_and_set_val(line, triggersStoploss, 'stoploss')
-
-                    # scrap_and_set_val(signal_instance, line, triggersCoin, 'coin')
-                    # scrap_and_set_val(signal_instance, line, triggersLever, 'lever')
-                    # scrap_and_set_val(signal_instance, line, triggersType, 'signal_type')
-                    # scrap_and_set_val(signal_instance, line, triggersStoploss, 'stoploss')
-
-                    #signal_instance.upload(mydb)
-                    #mydb.mycol.insertMany(signal_instance.__dict__)
-                    #print('***')
-            ## DEV ONLY ##
-            # print('do yo want to display it?')
-            # answ = input()
-            # if (answ == '1'):
-            #     print(channelMessages[i].message)
-            ## DEV END ##
-        i=i+1
-
-def scrap_and_set_val(_class, line, _dict, value):
-    for keyword in _dict:
-                    if line.find(keyword):
-                        try:
-                            _class.value = line.split(':')[1].upper()
-                        except IndexError:
-                            _class.value = line.upper()
-                        except:
-                            print('Throwing unknown exeption in scrap_and_set_val')
-def isTriggered(triggers, message):
-    for trigger in triggers:
-        if (trigger in message):
+def Find(obj, str):                            #Модифицированный поиск. Можно было воспользоваться "in", но магическим образом .find() работает лучше
+    try:                                       #Поэтому пришлось писать костыль, может в новых версиях Python исправят
+        if obj.find(str) == -1:                
+            return False
+        else:
             return True
-def create_signal(message):
-    pass
-main()
+    except:
+        return False
+        ### Если не находим, возращаем ЛОЖЬ, если находим ИСТИНА, если ошибка, то ЛОЖЬ
+
+main()  #Вызов главной функции
